509c501242bb5af3db208ad7494b4ceb4113b18b
Fawn <faithannwaringa@gmail.com>
2026-09-13 22:30:26 +0300

    feat: implemented ticket assignment and sols

--- models/employee.py
+++ models/employee.py
@@ -20,6 +20,15 @@ def assign_ticket(self, ticket_id):
         if ticket_id not in self._assigned_tickets:
             self._assigned_tickets.append(ticket_id)
 
+def to_dict(self):
+      data=super().to_dict()
+      data.update({
+            "type":"Employee",
+            "department":self.department,
+            "assigned_tickets":self._assigned_tickets
+      })
+      return data
+
    
 
 
